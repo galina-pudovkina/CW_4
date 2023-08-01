6eeb8e753063c75428a774dc60c0a8a202319d64
Galina Pudovkina <galinapudovkina8@mail.ru>
2023-08-01 23:15:17 +0300

    changes in vacancy

--- models/vacancy.py
+++ models/vacancy.py
@@ -2,11 +2,19 @@ class Vacancy:
     """
     Класс для представления вакансии
     """
+    vac_all = []
+
     def __init__(self, title, url, salary_from, employer):
         self.employer = employer
         self.salary_from = int(salary_from)
         self.url = url
         self.title = title
+        self.vac_all.append({
+            "Название профессии": self.title,
+            "Работодатель": self.employer,
+            "Зарплата от": self.salary_from,
+            "Ссылка на вакансию": self.url
+        })
 
     def __str__(self):
         return f"{self.title} {self.salary_from} ({self.url})"
